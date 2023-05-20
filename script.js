bee23ac0036c4cec52eccf257bf35670dee5b56a
Kpeguero16 <khalilpeguero@hotmail.com>
2023-05-19 22:09:38 -0400

    PICTURES WORK!!!

--- script.js
+++ script.js
@@ -1,59 +1,101 @@
 document.querySelector("#search").addEventListener("click", getPokemon);
 
+
+
+var ctx = document.getElementById("myChart").getContext("2d");
+  var myChart = new Chart(ctx, {
+    type: 'radar',
+    data: {
+      labels: ['HP','Attack', 'Defense', 'S-Attack', 'S-Defense', 'Speed'],
+      datasets: [{
+        label: 'Dataset 1',
+        data: [78, 85, 78, 109, 85, 100],
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scale: {
+        ticks: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
 function getPokemon(e)
 {
-    const name = document.querySelector("#pokemonName").value;
-
-    fetch('https://pokeapi.co/api/v2/pokemon/' + name)
-    .then((response) => response.json())
-    .then((data) => {
-        document.querySelector(".info").innerHTML = `
-        <div class="pokemonImage">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1008.png">
-        </div>
-        <div class="pokemonImage">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1008.png">
-        </div>
-        <div class="pokemonImage">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png">
-        </div>
-        <div class="pokemonImage">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png">
-        </div>
-        <div class="pokemonImage">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png">
-        </div>
-        <div class="pokemonImage">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png">
-        `;
+  const name = document.querySelector("#pokemonName").value;
+  document.querySelector("#pokemonName").value = '';
+  if(name != "")
+  {
+    getDefaultSprite(name.toLowerCase()).then(spriteUrl => {
+      document.getElementById("defaultSprite").src = spriteUrl;
     })
-    .catch((err) => {
-        console.log("Pokemon not found", err);
-    });
 
-    e.preventDefault();
+    getShinySprite(name.toLowerCase()).then(spriteUrl => {
+      document.getElementById("shinySprite").src = spriteUrl;
+    })
+  }
+  else
+  {
+    notFound();
+  }
+
+  
+
+  // document.querySelector(".info").innerHTML =
+  // `
+  // <div class="pokemonImage">
+  //   <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/395.png">
+  // </div>
+  // <div>
+  //   <canvas id="myChart"></canvas>
+  // </div>
+  // <div class="pokemonImage">
+  //   <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/395.png">
+  // </div>
+  // `;
+  
+  
 
 }
 
-var ctx = document.getElementById("myChart").getContext("2d");
-var myChart = new Chart(ctx, {
-  type: 'radar',
-  data: {
-    labels: ['HP','Attack', 'Defense', 'S-Attack', 'S-Defense'],
-    datasets: [{
-      label: 'Dataset 1',
-      data: [126, 131, 95, 131, 98],
-      backgroundColor: 'rgba(255, 99, 132, 0.2)',
-      borderColor: 'rgba(255, 99, 132, 1)',
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scale: {
-      ticks: {
-        beginAtZero: true
-      }
-    }
+async function getDefaultSprite(a) {
+  // const name = document.querySelector("#pokemonName").value;
+  try {
+    console.log("https://pokeapi.co/api/v2/pokemon/"+ a)
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/"+ a);
+    const data = await response.json();
+    const spriteUrl = data.sprites.front_default;
+    console.log(spriteUrl);
+    return spriteUrl;
+  } catch (error) {
+    console.log('Error:', error);
+    notFound();
   }
-});
+}
 
+async function getShinySprite(a) {
+  // const name = document.querySelector("#pokemonName").value;
+  try {
+    console.log("https://pokeapi.co/api/v2/pokemon/"+ a)
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/"+ a);
+    const data = await response.json();
+    const spriteUrl = data.sprites.front_shiny;
+    console.log(spriteUrl);
+    return spriteUrl;
+  } catch (error) {
+    console.log('Error:', error);
+    notFound();
+  }
+}
+
+function notFound() 
+{
+  document.getElementById("name_en").textContent = "Not found";
+  document.getElementById("name_jp").textContent = "Text box might be empty :)";
+  document.getElementById("defaultSprite").src = "not_found.png";
+  document.getElementById("shinySprite").src = "not_found.png";
+}
